d1a516d18efcc25f4b57fcf99ccc4dfdf1ca6ef6
florianeVisage <floriane@visage.jobs>
2022-05-16 17:16:21 +0200

    refactor: update WA version and add office's name

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,31 @@
-/// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
+// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
-import {bootstrapExtra} from "@workadventure/scripting-api-extra";
-
-// The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
-bootstrapExtra().catch(e => console.error(e));
+console.log('Script started successfully');
 
 let currentPopup: any = undefined;
+const deskNames: string[] = ['Nebulae', 'Equinox', 'Pulsar', 'Lionel']
 
-WA.room.onEnterLayer('clockZone').subscribe(() => {
-    const today = new Date();
-    const time = today.getHours() + ":" + today.getMinutes();
-    currentPopup = WA.ui.openPopup("clockPopup","It's " + time,[]);
-})
+// Waiting for the API to be ready
+WA.onInit().then(() => {
+    console.log('Scripting API ready');
 
-WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
+    for (const name of deskNames) {
+        WA.room.onEnterLayer(name + 'OfficeTrigger').subscribe(() => {
+            currentPopup = WA.ui.openPopup(name+'Title', `Welcome to ${name} office`, [])
+        })
 
-function closePopUp(){
+        WA.room.onLeaveLayer(name + 'OfficeTrigger').subscribe(closePopUp)
+    }
+
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    bootstrapExtra().then(() => {
+        console.log('Scripting API Extra ready');
+    }).catch(e => console.error(e));
+
+}).catch(e => console.error(e));
+
+function closePopUp() {
     if (currentPopup !== undefined) {
         currentPopup.close();
         currentPopup = undefined;
